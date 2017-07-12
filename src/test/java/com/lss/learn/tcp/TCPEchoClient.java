7f9c5290885cb2f5089034201dd546191fb3eeb2
Seanliu <909002538@qq.com>
2017-07-12 11:14:08 +0800

    tcp demo

---
+++ src/test/java/com/lss/learn/tcp/TCPEchoClient.java
@@ -0,0 +1,49 @@
+package com.lss.learn.tcp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Created by Sean on 2017/7/12.
+ */
+public class TCPEchoClient {
+    public static final String host ="";
+    public static final int port = 5555;
+    public static final String msg = "hello server";
+
+    public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket(host,port);
+            System.out.println("connected to server");
+
+            InputStream is = clientSocket.getInputStream();
+            OutputStream os = clientSocket.getOutputStream();
+
+            byte[] data = msg.getBytes();
+            os.write(data);
+            os.flush();
+
+
+            int totalByteReceive = 0;
+            int byteReceive = 0;
+
+            do {
+                byteReceive = is.read(data,totalByteReceive,data.length-totalByteReceive);
+                if (byteReceive == -1){
+                    System.out.println("connection closed");
+                    break;
+                }
+                totalByteReceive+=byteReceive;
+            }while (totalByteReceive < data.length);
+
+            System.out.println("receive:"+new String(data));
+
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/lss/learn/tcp/TCPEchoServer.java
@@ -0,0 +1,42 @@
+package com.lss.learn.tcp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Sean on 2017/7/12.
+ */
+public class TCPEchoServer {
+
+    private static final int port = TCPEchoClient.port;
+    private static final int msgLength = TCPEchoClient.msg.getBytes().length;
+
+    public static void main(String[] args) {
+        byte[] data = new byte[msgLength];
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = null;
+            while (true) {
+                socket = serverSocket.accept();
+                System.out.println("remote:"+socket.getRemoteSocketAddress());
+                InputStream is = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
+                int byteReceive = 0;
+                do {
+                    byteReceive = is.read(data);
+                    if (byteReceive !=-1){
+                        os.write(data,0,byteReceive);
+                    }
+                }while (byteReceive !=-1);
+
+                socket.close();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
